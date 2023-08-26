9bd9686e2963c453d572d039239a4bfc4872e418
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-08-26 10:45:10 +0530

    learning terraform

--- variables/variables.tf
+++ variables/variables.tf
@@ -6,6 +6,10 @@ variable "sample1"{
 default="Hello world"
 }
 
+variable "sample2"{
+default=true
+}
+
 
 output "demo"{
 value=var.sample
@@ -16,5 +20,9 @@ value=var.sample1
 }
 
 output "demo2"{
-value="value of sample & sample1 is - ${var.sample} & ${var.sample1}"
+value=var.sample2
+}
+
+output "demo2"{
+value="value of sample,sample1 & sample2 is - ${var.sample},${var.sample1}&${var.sample2}"
 }
