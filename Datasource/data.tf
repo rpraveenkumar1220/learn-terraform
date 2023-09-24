c8c1c9832d69759e9d5f3b73f2100d0f3a055d65
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-24 12:41:00 +0530

    learning terraform

--- Datasource/data.tf
+++ Datasource/data.tf
@@ -1,12 +1,8 @@
-data "aws_instances" "test" {
-
-  instance_state = ["stopped"]
-
-  instance_tags = {
-    Name = "workstation"
-  }
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.medium"
+  availability_zone = "us-east-1"
 }
 
-output "demo"{
-  value = length(data.aws_instances.test.instance_state)
+  output "demo"{
+  value = data.aws_ec2_spot_price.example.spot_price
 }
