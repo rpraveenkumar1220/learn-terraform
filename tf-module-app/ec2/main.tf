4e74bb1cc900a069aa509ffa8e3d36b545e7c89b
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-25 16:43:38 +0530

    learning terraform

---
+++ tf-module-app/ec2/main.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "instance" {
+  most_recent = true
+  owners = ["751367052640"]
+  name_regex = "centos-8-DevOps-Practice"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.instance.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Test"
+  }
+}
+
+resource "aws_security_group" "SG" {
+  name        = "Test_SG"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = " SSH "
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
