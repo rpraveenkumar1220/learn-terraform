8529cefa526b08ded023a98b8074c86e19ad688b
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-25 18:54:34 +0530

    learning terraform

--- tf-module-app/ec2/main.tf
+++ tf-module-app/ec2/main.tf
@@ -7,6 +7,7 @@ data "aws_ami" "instance" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.instance.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.SG.id]
 
   tags = {
     Name = "Test"
@@ -37,3 +38,5 @@ resource "aws_security_group" "SG" {
     Name = "allow_all"
   }
 }
+
+
