720507ed85507839cc8007d11e89e823a5183781
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-25 16:19:12 +0530

    learning terraform

--- Datasource/data.tf
+++ Datasource/data.tf
@@ -33,6 +33,10 @@ data "aws_instances" "test1" {
    }
 
 output "demo3"{
-  value = [ data.aws_instances.test1.private_ips, data.aws_instances.test1.public_ips ,  data.aws_instances.test1.instance_state_names ]
+  value = [ data.aws_instances.test1.private_ips, data.aws_instances.test1.public_ips ]
+}
+
+output "demo4"{
+   value = data.aws_instances.test1.instance_state_names
 }
 
