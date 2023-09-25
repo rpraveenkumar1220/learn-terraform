585a1deb544abd82bae8808bacebe70c0846f4be
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-25 15:46:38 +0530

    learning terraform

--- Datasource/data.tf
+++ Datasource/data.tf
@@ -1,6 +1,11 @@
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t3.medium"
   availability_zone = "us-west-2a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
 }
 
   output "demo"{
