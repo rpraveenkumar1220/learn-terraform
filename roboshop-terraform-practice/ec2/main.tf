701218e26fe683e606613dc463acb0e981f5456b
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-09-30 12:26:36 +0530

    Roboshop-ansible setup

--- roboshop-terraform-practice/ec2/main.tf
+++ roboshop-terraform-practice/ec2/main.tf
@@ -2,6 +2,7 @@ data "aws_ami" "instance" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
   owners     = ["973714476881"]
+
 }
 
 resource "aws_instance" "web" {
--- roboshop-terraform-practice/main.tf
+++ roboshop-terraform-practice/main.tf
@@ -1,49 +1,21 @@
-module "frontend"{
+module "instances"{
+  for_each = var.instances
   source =  "./ec2"
-  name = "frontend"
+  name = each.key
 }
 
-module "catalogue"{
-  source =  "./ec2"
-  name = "catalogue"
+variable "instances" {
+  default = {
+    frontend{}
+  mongodb {}
+  catalogue {}
+  redis {}
+  cart {}
+  user {}
+  mysql {}
+  payment {}
+  rabbitmq {}
+  shipping {}
 }
-
-module "mongodb"{
-  source =  "./ec2"
-  name = "mongodb"
-}
-
-module "user"{
-  source =  "./ec2"
-  name = "user"
-}
-
-module "cart"{
-  source =  "./ec2"
-  name = "cart"
-}
-
-module "redis"{
-  source =  "./ec2"
-  name = "redis"
-}
-
-module "mysql"{
-  source =  "./ec2"
-  name = "mysql"
-}
-
-module "shipping"{
-  source =  "./ec2"
-  name = "shipping"
-}
-
-module "payment"{
-  source =  "./ec2"
-  name = "payment"
-}
-
-module "rabbitmq"{
-  source =  "./ec2"
-  name = "rabbitmq"
+  }
 }
