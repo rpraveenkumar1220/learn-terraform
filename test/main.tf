68def1da91c65c49a8cfc1641e98c1e9c5cdad8c
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2023-10-13 22:23:06 +0530

    Roboshop with terraform setup

---
+++ test/main.tf
@@ -0,0 +1,17 @@
+vpc={
+  main={
+    cidr_block = "10.10.0.0/16"
+    subnets = {
+      public =    { cidr_block = ["10.10.1.0/24", "10.10.2.0/24"]   }
+      web    =    { cidr_block = ["10.10.3.0/24", "10.10.4.0/24"]   }
+      app    =    { cidr_block = ["10.10.5.0/24", "10.10.6.0/24"]   }
+      db     =    { cidr_block = ["10.10.7.0/24", "10.10.8.0/24"]   }
+    }
+  }
+}
+
+variable "vpc" {}
+
+output "test" {
+  value = var.vpc["cidr_block"]
+}
